4eb772dfd3746d7b84807e6d17ed6c15575f15ab
Austin P Mason <austinpmason@gmail.com>
2016-03-24 13:59:34 +0000

    telephone parser working

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -12,6 +12,16 @@ def legislators_by_zipcode(zipcode)
 	Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
+def clean_telephone(telephone)
+	telephone.split("").each {|num| num.gsub!(/[^0-9]/, '')}.join('').to_i
+end
+
+def can_sign_for_text_service?(telephone)
+	return true if telephone.length == 10
+	return true if telephone.length == 11 && telephone.split("")[0].to_i == 1
+	return false
+end
+
 def save_thank_you_letters(id,form_letter)
 	Dir.mkdir("output") unless Dir.exists? ("output")
 
@@ -34,6 +44,8 @@ contents.each do |row|
 	name = row[:first_name]
 	zipcode = clean_zipcode(row[:zipcode])
 	legislators = legislators_by_zipcode(zipcode)
+	telephone = clean_telephone(row[:homephone])
+	text_service = can_sign_for_text_service?(telephone.to_s)
 
 	form_letter = erb_template.result(binding)
 
