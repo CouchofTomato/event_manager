ed6dac4a4f4f81a9a45392210c519e417bfe4feb
Austin P Mason <austinpmason@gmail.com>
2016-03-30 14:28:14 +0100

    working hour and weekday calculation methods

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -26,6 +26,10 @@ def can_sign_for_text_service?(telephone)
 	telephone.length == 10 ? true : false
 end
 
+def parse_date(date)
+	DateTime.strptime(date, '%m/%d/%Y %H:%M')
+end
+
 def save_thank_you_letters(id,form_letter)
 	Dir.mkdir("output") unless Dir.exists? ("output")
 
@@ -43,6 +47,9 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
+hours = Hash.new(0)
+weekdays = Hash.new(0)
+
 contents.each do |row|
 	id = row[0]
 	name = row[:first_name]
@@ -50,10 +57,19 @@ contents.each do |row|
 	legislators = legislators_by_zipcode(zipcode)
 	telephone = clean_telephone(row[:homephone])
 	text_service = can_sign_for_text_service?(telephone.to_s)
-	print "#{telephone}: #{text_service}\n"
+	hour_registered = parse_date(row[:regdate]).hour
+	weekday_registered = parse_date(row[:regdate]).wday
+	hours[hour_registered] += 1
+	weekdays[weekday_registered] += 1
 
 	form_letter = erb_template.result(binding)
 
 	save_thank_you_letters(id,form_letter)
 
 end
+
+sorted_hours = hours.sort_by {|key, value| value}.reverse.to_h
+sorted_hours.each {|key, value| puts "#{key}: #{value}"}
+puts
+sorted_weekdays = weekdays.sort_by{|key, value| value}.reverse.to_h
+sorted_weekdays.each {|key, value| puts "#{key}: #{value}"}
